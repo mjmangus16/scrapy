1e7fbc0cba65239b391db5cf850a5d408d997e10
Michael Mangus <mjmangus16@yahoo.com>
2020-02-07 15:11:03 -0500

    cleaned up some files, comments, better naming conventions

--- postscrape/postscrape/spiders/get_html_spider.py
+++ postscrape/postscrape/spiders/get_html_spider.py
@@ -1,7 +1,17 @@
 import scrapy
 import json
 
-failed = []
+# ACTIVATE VENV
+# --- cd into top postscrape folder
+# --- source venv/bin/activate
+
+# RUN SCRAPER
+# --- scrapy crawl *name*
+# To write to a json file
+# --- scrapy crawl *name* -0 *name of json file*
+
+# This spider will grab the html of a specific page
+# This is a great place to start as it is easier to view the layout of the page and find the correct css selectors for grabbing the data you want.
 
 
 class PostsSpider(scrapy.Spider):
