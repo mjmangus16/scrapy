ee490df30f4fbc88853f12d7fda27800ac3118a6
mjmangus16 <mjmangus16@yahoo.com>
2020-02-18 15:59:58 -0500

    completed spider to grab inbounds data with a cleaned easy to read result

--- postscrape/postscrape/spiders/get_inbounds_data_spider.py
+++ postscrape/postscrape/spiders/get_inbounds_data_spider.py
@@ -9,7 +9,7 @@ import json
 # RUN SCRAPER
 # --- scrapy crawl *name*
 # To write to a json file
-# --- scrapy crawl *name* -0 *name of json file*
+# --- scrapy crawl *name* -o *name of json file*
 
 # This spider will grab the html of a specific page
 # This is a great place to start as it is easier to view the layout of the page and find the correct css selectors for grabbing the data you want.
@@ -23,12 +23,28 @@ class PostsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        allData = response.css("#inFlightGuide td::text").getall()
-        
+        allData1 = response.css("td.l input::attr(value)").getall()
+        allData2 = response.css("td.lp input::attr(value)").getall()
 
-        string = " ".join(allData)
-        
-        b = open("postscrape/spiders/inboundsData.txt", 'a+')
+        allData = allData1 + allData2
+        splitData = list()
 
-        b.write(string)
-            
+        for d in allData:
+            cont = d.split("|")
+            splitData.append(cont)
+
+        for d in splitData:
+
+            yield {
+                'title': d[1],
+                'inboundRating': f"{d[3]}/{d[4]}/{d[5]}/{d[6]}"
+            }
+
+                    
+        # b = open("postscrape/spiders/inboundsData2.txt", 'a+')
+
+        # for l in allData1:
+        #     b.write(f"'{l}',\n")
+
+        # for l in allData2:
+        #     b.write(f"'{l}',\n")
